385356bc79df21df3e637e415b1fb200605ddc5d
selasiehanson <soundfever18@gmail.com>
2013-05-15 18:24:43 +0000

    basic setup for testing working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,17 @@
 var app  = angular.module("AxDxts",[]);
+
+app.value("version","0.0.1");
+
+app.controller("MainCtrl", ["$scope", function ($scope){
+	$scope.users = [];
+	
+	$scope.addUser = function (name){
+		if(name){
+			$scope.users.push(name);
+		}
+	}
+}]);
+
+app.directive("greeter", function (){
+
+});
--- test/spec/main.js
+++ test/spec/main.js
@@ -1,9 +1,40 @@
 'use strict';
 
 describe("Main Controller", function (){
+	var mainModule;
+	var scope, controller;
 	beforeEach(module("AxDxts"));
 
-	it("should have a main controller", function (){
-		expect(AxDxts.MainCtrl).not.toEqual(null);
+	it("should have a main controller", inject(function (version,$rootScope, $controller){
+		
+		beforeEach(function (){
+			scope = $rootScope.$new();
+			
+			controller = $controller("MainCtrl",{
+				$scope : scope
+			});
+		});
+
+		expect(version).toEqual("0.0.1");
+
+	}));
+
+	describe("Adding new users", function (){
+		it("should have an 'addUser' method", function (){
+			expect(scope.addUser).not.toEqual(null);
+		});
+		
+		it("should add a new user when a user is present", function (){
+			expect(scope.users.length).toEqual(0);
+			scope.addUser("kweku")
+			expect(scope.users.length).toEqual(1);
+		});
+
+		it("should NOT add a new user when a user is absent", function (){
+			expect(scope.users.length).toEqual(0);
+			scope.addUser();
+			expect(scope.users.length).not.toEqual(1);
+		});
+
 	});
 });
